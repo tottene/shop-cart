606095228396c59a5786038fefbe04b27fa4d4c3
Cleiton Tottene <cleiton.rio@gmail.com>
2023-05-27 09:01:23 -0300

    v1.0.0

---
+++ readme.md
@@ -0,0 +1,3 @@
+# shop-cart
+
+https://www.figma.com/file/7yJJmBKe8PD4RHUrLtShOC/Carrinho-de-compras-%E2%80%A2-Desafio-21-(Community)?type=design&node-id=322-2&t=vaHpLIL3JdUAsrMW-0
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center max-w-[1280px] bg-black h-screen my-0 mx-auto">
+      <h1>Shop Cart</h1>
+      <h1>Shop Cart 2</h1>
+    </main>
+  )
+}
